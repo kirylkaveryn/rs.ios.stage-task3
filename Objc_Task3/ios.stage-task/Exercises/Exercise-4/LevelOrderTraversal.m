29bfc4086db1e88d0dedcee51c14bc92f4dd9c6f
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2021-05-24 17:39:36 +0300

    feat: final task3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,5 +1,108 @@
 #import "LevelOrderTraversal.h"
 
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    return nil;
+    
+    NSInteger arrayLength = [tree count];
+    NSInteger currentLevel = -1;
+    NSInteger levels = -1;
+    NSInteger rightBranchLevel = 0;
+    NSInteger branch = 0;
+//    NSInteger left = 0;
+    
+    if (arrayLength == 0) {
+        return @[];
+    }
+    
+    if (arrayLength == 1 && tree[0] == [NSNull null]) {
+        return @[];
+    }
+    
+    if (arrayLength == 1 && tree[0] != [NSNull null]) {
+        NSMutableArray *outputArrayLength1 = [NSMutableArray new];
+        [outputArrayLength1 addObject:tree];
+        return outputArrayLength1;
+    }
+    
+    NSInteger i = 0;
+    
+    while (i <  arrayLength) {
+        if (tree[i] == [NSNull null]) {
+            i++;
+            rightBranchLevel ++;
+            if (tree[i] == [NSNull null]) {
+                currentLevel = currentLevel - rightBranchLevel;
+                rightBranchLevel = 0;
+//                currentLevel --;
+                i++;
+            }
+        }
+        if (tree[i] != [NSNull null]) {
+            currentLevel ++;
+            if (levels <= currentLevel) {
+                levels = currentLevel;
+            }
+            i++;
+        }
+    }
+    
+    NSLog(@"levels: %li", levels);
+
+    NSMutableArray *outputArray = [NSMutableArray new];
+    for (int i = 0; i <= levels; i++) {
+        [outputArray addObject:[NSMutableArray new]];
+    }
+    
+    currentLevel = 0 ;
+    i = 0;
+    
+    while (i <  arrayLength) {
+        if (tree[i] == [NSNull null]) {
+            i++;
+//            rightBranchLevel ++;
+            if (i < arrayLength && tree[i] == [NSNull null]) {
+//                currentLevel = currentLevel - rightBranchLevel;
+                rightBranchLevel = 0;
+                currentLevel --;
+                i++;
+            }
+//            else if (i < arrayLength && tree[i] != [NSNull null]) {
+//                [[outputArray objectAtIndex:currentLevel] addObject:tree[i]];
+//                currentLevel ++;
+//                i++;
+//            }
+        }
+        if (i < arrayLength && tree[i] != [NSNull null]) {
+            [[outputArray objectAtIndex:currentLevel] addObject:tree[i]];
+            currentLevel ++;
+            i++;
+        }
+    }
+
+    NSLog(@"outputArray: %@", outputArray);
+    
+    return outputArray;
 }
+
+
+//while (i < arrayLength) {
+//
+//    if (i == arrayLength - 2 && tree[arrayLength - 1] != [NSNull null]) {
+//        [[outputArray objectAtIndex:currentLevel] addObject:tree[arrayLength - 1]];
+//        currentLevel ++;
+//        i ++;
+//    }
+//    else if (tree[i] == [NSNull null] && tree[i + 1] == [NSNull null]) {
+//        currentLevel --;
+//        i += 2;
+//    }
+//    else if (tree[i] == [NSNull null] && tree[i + 1] != [NSNull null]) {
+//        [[outputArray objectAtIndex:currentLevel] addObject:tree[i+1]];
+//        currentLevel ++;
+//        i ++;
+//    }
+//    else {
+//        [[outputArray objectAtIndex:currentLevel] addObject:tree[i]];
+//        currentLevel ++;
+//        i ++;
+//    }
+//}
---
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/preorderTraversal.m
@@ -0,0 +1,24 @@
+////
+////  preorderTraversal.m
+////  ios.stage-task
+////
+////  Created by Kirill on 5/23/21.
+////
+//
+//#import "preorderTraversal.h"
+//
+//
+//
+//@implementation preorderTraversal
+//
+//- (NSMutableArray*) preorderTraversal: (TreeNode *)treeNode andArray:(NSMutableArray *)inputArray {
+//    
+//    if (treeNode != nil) {
+//        
+//        
+//    }
+//    
+//}
+//
+//
+//@end
